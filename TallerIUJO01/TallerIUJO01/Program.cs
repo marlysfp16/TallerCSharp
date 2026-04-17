eddbc68eeb0a3b41f99caa464c90cb3c58bcc9b8
marlysfp16 <marlysfp16@gmail.com>
2026-04-17 11:59:07 -0400

    revision de error de archivo

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -1,12 +1,5 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: usuario
- * Fecha: 17/4/2026
- * Hora: 10:53 a. m.
- * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
- */
-using System;
+﻿using System;
+using System.IO;
 
 namespace TallerIUJO01
 {
@@ -15,18 +8,19 @@ namespace TallerIUJO01
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("===Taller 01===");
-			
+			Console.WriteLine("\n");
 			
 			//1. El dato del usuario
 			
 			string registroUsuario = "    ID_666   ; marlysflores ;  EVALUACION   ;   95";
 			
-			Console.WriteLine(registroUsuario);
 			
 			string registroLimpio = registroUsuario.Trim().Trim();
 			
 		
 			Console.WriteLine(registroLimpio);
+			
+			Console.WriteLine("\n");
 			
 			string[] partes = registroLimpio.Split(';');
 			string id = partes[0].Trim();
@@ -34,10 +28,37 @@ namespace TallerIUJO01
 			string tarea = partes[2].Trim();
 			string nota = partes[3].Trim();
 			
-			Console.WriteLine(string.Format("El id es: {0} \n Usuario: {1} \n La nota {2} \n Evaluacion: {3}", id, nombre, nota, tarea));
+			Console.WriteLine("=================================");
+			Console.WriteLine(string.Format(" El id es: {0} \n Usuario: {1} \n La nota {2} \n Evaluacion: {3}", id, nombre, nota, tarea));
+			Console.WriteLine("=================================");
+			
+			Console.WriteLine("\n");
+			
+			//flujo de archivos
+			
+			string rutaraiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"DatosIUJO");
+			
+			if(!Directory.Exists("Reportes"))
+			{
+				Directory.CreateDirectory("Reportes");
+				
+				Console.WriteLine("Directorio Creado con exito");
+			}
+			
+			string archivotexto =Path.Combine(rutaraiz,"notas.txt");
 			
 			
 			
+			Console.WriteLine("\n");
+			
+			Console.WriteLine(archivotexto);
+			
+			Console.WriteLine("\n");
+			
+			using(StreamWriter sw = new StreamWriter(archivotexto, true))
+			{
+				sw.WriteLine(string.Format("ID : {0} Nota {1} Fecha: {yyyy-MM-dd-HH:mm}", id, nota, DateTime.Now));
+			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);

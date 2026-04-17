3a5a55164e9ba94b6047585b54117a5c856c5ae7
marlysfp16 <marlysfp16@gmail.com>
2026-04-17 11:29:59 -0400

    agregando manipulacion de texto

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -14,9 +14,30 @@ namespace TallerIUJO01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("===Taller 01===");
 			
-			// TODO: Implement Functionality Here
+			
+			//1. El dato del usuario
+			
+			string registroUsuario = "    ID_666   ; marlysflores ;  EVALUACION   ;   95";
+			
+			Console.WriteLine(registroUsuario);
+			
+			string registroLimpio = registroUsuario.Trim().Trim();
+			
+		
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			Console.WriteLine(string.Format("El id es: {0} \n Usuario: {1} \n La nota {2} \n Evaluacion: {3}", id, nombre, nota, tarea));
+			
+			
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
